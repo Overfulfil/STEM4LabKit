1e6de9e2f59752cedc97acbeb662b33f7875ea3f
larryyee2005 <32700530+larryyee2005@users.noreply.github.com>
2020-06-20 12:14:58 +0800

    Update main.ts

--- main.ts
+++ main.ts
@@ -362,5 +362,33 @@ namespace STEMLab {
     export function Address(addr: BME280_I2C_ADDRESS) {
         BME280_I2C_ADDR = addr
     }
+	
+//Soil
+    //% blockId="readMoisture" block="Soil Moisture (0-4096)"
+    //% blockGap=2 weight=79
+    export function readMoisture(): number {
+   	pins.i2cWriteNumber(81,0,NumberFormat.Int8LE,false)
+	return (4096 - pins.i2cReadNumber(81, NumberFormat.UInt16BE, false))    
+    }
+	
+    //% blockId="soilTooDry" block="Soil too dry?"
+    //% blockGap=2 weight=79
+    export function soilTooDry(): boolean {
+   	pins.i2cWriteNumber(81,0,NumberFormat.Int8LE,false)
+	if ((4096 - pins.i2cReadNumber(81, NumberFormat.UInt16BE, false)) < 100) {
+		return true    
+	}
+	return false
+    }
+
+    //% blockId="soilTooWet" block="Soil too Wet?"
+    //% blockGap=2 weight=79
+    export function soilTooWet(): boolean {
+   	pins.i2cWriteNumber(81,0,NumberFormat.Int8LE,false)
+	if ((4096 - pins.i2cReadNumber(81, NumberFormat.UInt16BE, false)) > 3000) {
+		return true    
+	}
+	return false
+    }
 
 }
