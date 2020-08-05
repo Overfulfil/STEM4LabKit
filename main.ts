e77e16ff6911a15ba984deae024086fa6e99614f
larryyee2005 <32700530+larryyee2005@users.noreply.github.com>
2020-08-05 10:51:34 +0800

    Update main.ts

--- main.ts
+++ main.ts
@@ -20,7 +20,7 @@ enum BME280_P {
 }
 
 //% weight=0 color=#3CB371 icon="\uf1b3" block="STEM For Lab Kit"
-//% groups=['Color', 'Gas', 'Pressure', 'Pressure', 'Moisture', 'OLED', 'EEPROM', 'others']
+//% groups=['Color', 'Gas', 'Pressure', 'Pressure', 'Moisture', 'OLED', 'EEPROM', "ThingSpeak"'others']
 namespace STEMLab {
 
     let RGB_OK = true
@@ -735,4 +735,194 @@ namespace STEMLab {
     	basic.pause(100)
     	return ret22
     }
+
+    //WiFi and ThingSpeak
+    let wifi_connected: boolean = false
+    let thingspeak_connected: boolean = false
+    let kitsiot_connected: boolean = false
+    let last_upload_successful: boolean = false
+    let userToken_def: string = ""
+    let topic_def: string = ""
+    let recevice_kidiot_text = ""
+    const EVENT_ON_ID = 100
+    const EVENT_ON_Value = 200
+    const EVENT_OFF_ID = 110
+    const EVENT_OFF_Value = 210
+    let toSendStr = ""
+
+    export enum State {
+        //% block="Success"
+        Success,
+        //% block="Fail"
+        Fail
+    }
+
+    // write AT command with CR+LF ending
+    function sendAT(command: string, wait: number = 0) {
+        serial.writeString(command + "\u000D\u000A")
+        basic.pause(wait)
+    }
+
+    // wait for certain response from ESP8266
+    function waitResponse(): boolean {
+        let serial_str: string = ""
+        let result: boolean = false
+        let time: number = input.runningTime()
+        while (true) {
+            serial_str += serial.readString()
+            if (serial_str.length > 200)
+                serial_str = serial_str.substr(serial_str.length - 200)
+            if (serial_str.includes("OK") || serial_str.includes("ALREADY CONNECTED") || serial_str.includes("WIFI GOT IP") || serial_str.includes("CONNECT")) {
+                result = true
+                break
+            }
+            if (serial_str.includes("ERROR") || serial_str.includes("FAIL")) {
+                break
+            }
+            if (input.runningTime() - time > 5000) {
+                break
+            }
+        }
+        return result
+    }
+    /**
+    * Initialize ESP8266 module 
+    */
+    //% block="set ESP8266|RX %tx|TX %rx|Baud rate %baudrate"
+    //% tx.defl=SerialPin.P8
+    //% rx.defl=SerialPin.P16
+    //% ssid.defl=your_ssid
+    //% pw.defl=your_password weight=100
+    //% group="ThingSpeak"
+    export function initWIFI(tx: SerialPin, rx: SerialPin, baudrate: BaudRate) {
+        serial.redirect(
+            tx,
+            rx,
+            baudrate
+        )
+        sendAT("AT+RESTORE", 1000) // restore to factory settings
+        sendAT("AT+CWMODE=1") // set to STA mode
+        sendAT("AT+RST", 1000) // reset
+        basic.pause(100)
+    }
+    /**
+    * connect to Wifi router
+    */
+    //% block="connect Wifi SSID = %ssid|KEY = %pw"
+    //% ssid.defl=your_ssid
+    //% pw.defl=your_pw weight=95
+    //% group="ThingSpeak"
+    export function connectWifi(ssid: string, pw: string) {
+
+        wifi_connected = false
+        thingspeak_connected = false
+        kitsiot_connected = false
+        sendAT("AT+CWJAP=\"" + ssid + "\",\"" + pw + "\"", 0) // connect to Wifi router
+        wifi_connected = waitResponse()
+        basic.pause(100)
+    }
+    /**
+    * Connect to ThingSpeak
+    */
+    //% block="connect thingspeak"
+    //% write_api_key.defl=your_write_api_key
+    //% subcategory="ThingSpeak" weight=90
+    //% group="ThingSpeak"
+    export function connectThingSpeak() {
+        if (wifi_connected && kitsiot_connected == false) {
+            thingspeak_connected = false
+            let text = "AT+CIPSTART=\"TCP\",\"api.thingspeak.com\",80"
+            sendAT(text, 0) // connect to website server
+            thingspeak_connected = waitResponse()
+            basic.pause(100)
+        }
+    }
+    /**
+    * Connect to ThingSpeak and set data. 
+    */
+    //% block="set data to send ThingSpeak | Write API key = %write_api_key|Field 1 = %n1||Field 2 = %n2|Field 3 = %n3|Field 4 = %n4|Field 5 = %n5|Field 6 = %n6|Field 7 = %n7|Field 8 = %n8"
+    //% write_api_key.defl=your_write_api_key
+    //% expandableArgumentMode="enabled"
+    //% subcategory="ThingSpeak" weight=85
+    //% group="ThingSpeak"
+    export function setData(write_api_key: string, n1: number = 0, n2: number = 0, n3: number = 0, n4: number = 0, n5: number = 0, n6: number = 0, n7: number = 0, n8: number = 0) {
+            toSendStr = "GET /update?api_key="
+                + write_api_key
+                + "&field1="
+                + n1
+                + "&field2="
+                + n2
+                + "&field3="
+                + n3
+                + "&field4="
+                + n4
+                + "&field5="
+                + n5
+                + "&field6="
+                + n6
+                + "&field7="
+                + n7
+                + "&field8="
+                + n8
+    }
+    /**
+    * upload data. It would not upload anything if it failed to connect to Wifi or ThingSpeak.
+    */
+    //% block="Upload data to ThingSpeak"
+    //% subcategory="ThingSpeak" weight=80
+    //% group="ThingSpeak"
+    export function uploadData() {
+        if (thingspeak_connected) {
+            last_upload_successful = false
+            sendAT("AT+CIPSEND=" + (toSendStr.length + 2), 100)
+            sendAT(toSendStr, 100) // upload data
+            last_upload_successful = waitResponse()
+            basic.pause(100)
+        }
+    }
+
+    /**
+    * Check if ESP8266 successfully connected to Wifi
+    */
+    //% block="Wifi connected %State" weight=70
+    //% group="ThingSpeak"
+    export function wifiState(state: boolean) {
+        if (wifi_connected == state) {
+            return true
+        }
+        else {
+            return false
+        }
+    }
+
+    /**
+    * Check if ESP8266 successfully connected to ThingSpeak
+    */
+    //% block="ThingSpeak connected %State"
+    //% subcategory="ThingSpeak" weight=65
+    //% group="ThingSpeak"
+    export function thingSpeakState(state: boolean) {
+        if (thingspeak_connected == state) {
+            return true
+        }
+        else {
+            return false
+        }
+    }
+
+    /**
+    * Check if ESP8266 successfully uploaded data to ThingSpeak
+    */
+    //% block="ThingSpeak Last data upload %State"
+    //% subcategory="ThingSpeak" weight=60
+    //% group="ThingSpeak"
+    export function tsLastUploadState(state: boolean) {
+        if (last_upload_successful == state) {
+            return true
+        }
+        else {
+            return false
+        }
+    }
+
 }
